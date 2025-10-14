db94f9ce48f5490e82029f32e036ec46447eefeb
Ntokozo <73021603+Ntokozo-bit@users.noreply.github.com>
2025-10-14 14:49:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ window.SITE = {
     NAME: 'RepurposerHub',
     URL: 'https://repurposerhub.org', // ← change to your real domain
     CONTACT_EMAIL: 'ntokozolms10@gmail.com',
-    GA_MEASUREMENT_ID: 'G-DJJNL418DX',     // ← your GA4 ID (change if needed)
+    GA_MEASUREMENT_ID: '',     // ← your GA4 ID (change if needed)
     ADSENSE_PUB_ID: 'ca-pub-7179154805169763' // ← keep this
 };
